ebd542603c188577bf7d19e08c80629707fd15e9
정연진 <y_jin99@naver.com>
2022-11-01 15:21:20 +0900

    FEAT : 공지사항 조회 (+페이지네이션)

---
+++ src/main/java/com/ssafy/live/model/dto/FileInfo.java
@@ -0,0 +1,12 @@
+package com.ssafy.live.model.dto;
+
+import lombok.Data;
+
+@Data
+public class FileInfo {
+
+	private String saveFolder;
+	private String originalFile;
+	private String saveFile;
+
+}
---
+++ src/main/java/com/ssafy/live/model/mapper/BoardMapper.java
@@ -0,0 +1,25 @@
+package com.ssafy.live.model.mapper;
+
+import java.sql.SQLException;
+import java.util.List;
+import java.util.Map;
+
+import org.apache.ibatis.annotations.Mapper;
+
+import com.ssafy.live.model.dto.Board;
+
+@Mapper
+public interface BoardMapper {
+	int insert(Board board) throws SQLException;
+
+//	void registerFile(Board board) throws Exception;
+	List<Board> selectAll(Map<String, Object> map) throws SQLException;
+	int getTotalArticleCount(Map<String, Object> map) throws SQLException;
+//	Board getArticle(int articleNo) throws SQLException;
+//	void updateHit(int articleNo) throws SQLException;
+//	void modifyArticle(Board boardDto) throws SQLException;
+//	void deleteFile(int articleNo) throws Exception;
+//	void deleteArticle(int articleNo) throws SQLException;
+//	List<FileInfo> fileInfoList(int articleNo) throws Exception;
+
+}
