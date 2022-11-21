8c3d7f5cf1455887070d676a5de6b013714ad3bb
sxot94 <sxot94@gmail.com>
2022-11-21 17:51:13 +0900

    FEAT : APTLIST

--- src/main/java/com/ssafy/live/model/mapper/AptMapper.java
+++ src/main/java/com/ssafy/live/model/mapper/AptMapper.java
@@ -11,6 +11,8 @@ import com.ssafy.live.model.dto.Interest;
 
 @Mapper
 public interface AptMapper {
+	// 아파트 상세 매매정보 조회
+	List<Apt> listDetailApt(Map<String, String> map) throws SQLException;
 	// 아파트 매매 정보 조회
 	List<Apt> listApt(Map<String, String> map) throws SQLException;
 	// 관심매물 등록
