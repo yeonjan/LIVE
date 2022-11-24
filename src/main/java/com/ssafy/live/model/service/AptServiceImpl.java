4d3238a075bed52c38b8aae1758be0bf41bcfeb3
김태수 <sxott94@gmail.com>
2022-11-24 11:12:32 +0900

    UPDATE : interestView

--- src/main/java/com/ssafy/live/model/service/AptServiceImpl.java
+++ src/main/java/com/ssafy/live/model/service/AptServiceImpl.java
@@ -48,5 +48,9 @@ public class AptServiceImpl implements AptService {
 		log.debug("관심 매물 삭제 서비스");
 		aptMapper.deleteInterest(aptCode);
 	}
+	@Override
+	public List<Apt> listInterestInfo(String userId) throws SQLException {
+		return aptMapper.listInterestInfo(userId);
+	}
 
 }
